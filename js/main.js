efe0d94e5752e59151dc3eb1273c795f9794240c
sibele-lino <sibelecaetano7@gmail.com>
2026-04-26 19:48:49 -0300

    feat: adiciona menu hamburguer com animação e acessibilidade

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//Seleciona os elementos do DOM
+const navToggle = document.querySelector('.nav-toggle');
+const mainNav = document.querySelector('#main-nav');
+const navLinks = document.querySelectorAll('.nav-links a');
+
+//Função que abre e fecha o menu de navegação
+function toggleMenu() {
+    //lê o estado "true" ou "false" (string, não booleano) do atributo "aria-expanded"
+    const isOpen = navToggle.getAttribute('aria-expanded') === 'true'
+
+    //alterna o estado do menu
+    navToggle.setAttribute('aria-expanded', !isOpen);
+
+    //adiciona ou remove a classe que mostra o menu
+    mainNav.classList.toggle('open');
+
+}
+
+//adiciona o evento clique no botão hambúrguer toggle
+navToggle.addEventListener('click', toggleMenu)
+
+//fecha o menu quando o usuário clicar em um link de navegação
+navLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+    navToggle.setAttribute('aria-expanded', 'false')
+    mainNav.classList.remove('nav--open')
+    })
+})
